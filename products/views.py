09fc84c25c0a057684996228da342efcad169723
laureno12 <laurenosays@gmail.com>
2023-02-21 20:36:10 -0500

    created urls in product project and products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def products_list(request):
+
+    return Response('ok')
+
 
 # Create your views here.
