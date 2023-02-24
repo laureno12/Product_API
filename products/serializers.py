ed9b5d65b1a702e99508f5094829a6c5752893cc
laureno12 <laurenosays@gmail.com>
2023-02-24 18:44:06 -0500

    Registered product mode with admin site + completed POST functionality for Product + Updated Serializer product class

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    model = Product
-    fields = ['title', 'description', 'price', 'inventory quantity']
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'price', 'inventory_quantity']
 
     
--- products/views.py
+++ products/views.py
@@ -1,10 +1,37 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serialzer = ProductSerializer(products,many=True)
+        return Response(serialzer.data, status=status.HTTP_200_OK)
+    
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+
+
+
+
+
+    # if request.method == 'GET':
+    #     product = Product.objects.all()
+    #     serializer = ProductSerializer(product, many = True)
+    #     return Response(serializer.data)
+    # elif request.method == 'POST':
+    #     pass
+    
+
 
 
 # Create your views here.
